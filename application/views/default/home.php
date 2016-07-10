81a47229f3eb7cf9b5530004a0cde6a56d6a9bff
Nguyen Van Bach <vanbach1213@gmail.com>
2016-07-10 18:00:58 +0900

    Thay doi thu tu cac module trong trang Home

--- application/views/default/home.php
+++ application/views/default/home.php
@@ -8,28 +8,9 @@ $(function(){
 
 <div class="container">
     <div class="panel panel-default">
-        <a class="panel-heading" href="<?=base_url()?>summary/viewlist" style="display:block">
-            <strong><span class="glyphicon glyphicon-menu-right pull-right"></span>Thu chi trong tháng (<?=date('Y-m')?>)</strong>
-        </a>
-        <table class="table table-bordered">
-            <tr>
-                <th style="width:33.3%" class="text-center">Thu</th>
-                <th style="width:33.3%" class="text-center">Chi</th>
-                <th style="width:33.3%" class="text-center">Chênh lệch</th>
-            </tr>
-            <tr>
-                <td class="text-right"><?=currency($month_sum['thu'])?></td>
-                <td class="text-right"><?=currency($month_sum['chi'])?></td>
-                <td class="text-right"><?=currency($month_sum['tong'])?></td>
-            </tr>
-        </table>
-    </div>
-</div>
-
-<div class="container">
-    <div class="panel panel-default">
         <a class="panel-heading" href="<?=base_url()?>setting/edit/month_outgo_plans" style="display:block">
-            <strong><span class="glyphicon glyphicon-menu-right pull-right"></span>Chi thực tế / Dự định</strong>
+            <strong><span class="glyphicon glyphicon-menu-right pull-right"></span>Chi thực tế / Dự định</strong><br>
+            <span class="small text-muted"><em>Không tính các khoản chi cố định</em></span>
         </a>
         <table class="table table-bordered">
             <tr>
@@ -51,6 +32,26 @@ $(function(){
                         </div>
                     </div>
                 </td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+<div class="container">
+    <div class="panel panel-default">
+        <a class="panel-heading" href="<?=base_url()?>summary/viewlist" style="display:block">
+            <strong><span class="glyphicon glyphicon-menu-right pull-right"></span>Thu chi trong tháng (<?=date('Y-m')?>)</strong>
+        </a>
+        <table class="table table-bordered">
+            <tr>
+                <th style="width:33.3%" class="text-center">Thu</th>
+                <th style="width:33.3%" class="text-center">Chi</th>
+                <th style="width:33.3%" class="text-center">Chênh lệch</th>
+            </tr>
+            <tr>
+                <td class="text-right"><?=currency($month_sum['thu'])?></td>
+                <td class="text-right"><?=currency($month_sum['chi'])?></td>
+                <td class="text-right"><?=currency($month_sum['tong'])?></td>
             </tr>
         </table>
     </div>
