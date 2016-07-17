a0964384aa2b2e86ca9092e8679654bb9413f87c
Nguyen Van Bach <vanbach1213@gmail.com>
2016-07-17 17:00:29 +0900

    Sua form nhap Inout record

--- application/views/default/inout/form.php
+++ application/views/default/inout/form.php
@@ -1,4 +1,6 @@
 <?php 
+
+// Xóa lựa chọn "Tiền mặt" nếu thao tác là Rút hoặc nạp tiền vào tài khoản
 if (in_array($type, array('drawer', 'deposit'))){
     unset($select['accounts'][Inout_model::ACCOUNT_CASH_ID]);
 }
@@ -9,6 +11,8 @@ $disabled_attr = (!empty($pair_id))? array('disabled' => 'true') : array();
 
 <script type="text/javascript">
     $(function(){
+        $(".autofocus").focus();
+        
         $('.del-record').click(function(evt){
             evt.preventDefault();
             if (confirm('Muốn xóa ghi chép này?')){
@@ -25,127 +29,134 @@ $disabled_attr = (!empty($pair_id))? array('disabled' => 'true') : array();
     });
 </script>
 
-<?php echo form_open($form_url, array('id' => 'addCashFlow', 'class' => 'container'))?>
-    <div class="panel panel-default">
-        <div class="panel-heading"><strong><?=$title?></strong></div>
-        <div class="panel-body">
-            <div class="form-group">
-                <label>Số tiền:</label>
-                <div class="input-group">
-                    <span class="input-group-addon"><?=$inout_type_sign?></span>
+<div class="container">
+    <?php echo form_open($form_url, array('id' => 'addCashFlow', 'class' => 'form-vertical'))?>
+        <div class="panel panel-default">
+            <div class="panel-heading"><strong><?=$title?></strong></div>
+            <div class="panel-body">
+                <div class="form-group">
+                    <label>Số tiền:</label>
+                    <div class="input-group">
+                        <span class="input-group-addon"><?=$inout_type_sign?></span>
+                        <?=form_input(
+                            array(
+                                'name' => $field_name = 'amount',
+                                'type' => 'number',
+                                'pattern' => '\d*',
+                            ),
+                            set_value($field_name, null),
+                            array(
+                                'class' => 'form-control autofocus',
+                            )
+                        )?>
+                        <span class="input-group-addon">¥</span>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Thời gian:</label>
                     <?=form_input(
                         array(
-                            'name' => $field_name = 'amount',
-                            'type' => 'number',
-                            'pattern' => '\d*',
-                            'autofocus' => 'autofocus',
+                            'name' => $field_name = 'date',
+                            'type' => 'date',
                         ),
-                        set_value($field_name, null),
+                        set_value($field_name, date('Y-m-d')),
                         array(
                             'class' => 'form-control',
                         )
                     )?>
-                    <span class="input-group-addon">¥</span>
                 </div>
-            </div>
-            <div class="form-group">
-                <label>Thời gian:</label>
-                <?=form_input(
-                    array(
-                        'name' => $field_name = 'date',
-                        'type' => 'date',
-                    ),
-                    set_value($field_name, date('Y-m-d')),
-                    array(
-                        'class' => 'form-control',
-                    )
-                )?>
-            </div>
-            
-            <?php if (in_array($type, array('outgo', 'income'))): ?>
-            <div class="form-group">
-                <label>Danh mục:</label>
-                <?=form_dropdown(
-                    $field_name = 'category_id', 
-                    $select['categories'], 
-                    set_value($field_name, null), 
-                    array(
-                        'class' => 'form-control',
-                    )
-                )?>
-            </div>
-            <?php endif ?>
-            
-            <?php if (in_array($type, array('outgo', 'income', 'drawer', 'deposit'))): ?>
-            <div class="form-group">
-                <label>Tài khoản:</label>
-                <?=form_dropdown(
-                    $field_name = 'account_id',
-                    $select['accounts'], 
-                    set_value($field_name, null), 
-                    array(
-                        'class' => 'form-control',
-                    ) + $disabled_attr
-                )?>
-            </div>
-            <div class="form-group">
-                <label>Phụ trách:</label>
-                <?=form_dropdown(
-                    $field_name = 'player', 
-                    $select['players'], 
-                    set_value($field_name, $this->login_model->getInfo('uid')), 
-                    array(
-                        'class' => 'form-control',
-                    )
-                )?>
-            </div>
-            <?php endif ?>
-            
-            <?php if (in_array($type, array('handover'))): ?>
-            <div class="form-group">
-                <div class="row">
-                    <div class="col-xs-6"><label>Chuyển từ:</label></div>
-                    <div class="col-xs-6"><label>đến:</label></div>
+                
+                <?php if (in_array($type, array('outgo', 'income'))): ?>
+                <div class="form-group">
+                    <label>Danh mục:</label>
+                    <?=form_dropdown(
+                        $field_name = 'category_id', 
+                        $select['categories'], 
+                        set_value($field_name, null), 
+                        array(
+                            'class' => 'form-control',
+                        )
+                    )?>
                 </div>
+                <?php endif ?>
+                
+                <?php if (in_array($type, array('outgo', 'income', 'drawer', 'deposit'))): ?>
                 <div class="row">
                     <div class="col-xs-6">
-                        <?=form_dropdown(
-                            $field_name = 'player[0]', 
-                            $select['players'], 
-                            set_value($field_name, $this->login_model->getInfo('uid')), 
-                            array(
-                                'class' => 'form-control',
-                            ) + $disabled_attr
-                        )?>
+                        <div class="form-group">
+                            <label>Tài khoản:</label>
+                            <?=form_dropdown(
+                                $field_name = 'account_id',
+                                $select['accounts'], 
+                                set_value($field_name, null), 
+                                array(
+                                    'class' => 'form-control',
+                                ) + $disabled_attr
+                            )?>
+                        </div>
                     </div>
                     <div class="col-xs-6">
-                        <?=form_dropdown(
-                            $field_name = 'player[1]', 
-                            $select['players'], 
-                            set_value($field_name, 3-$this->login_model->getInfo('uid')), 
-                            array(
-                                'class' => 'form-control',
-                            ) + $disabled_attr
-                        )?>
+                        <div class="form-group">
+                            <label>Phụ trách:</label>
+                            <?=form_dropdown(
+                                $field_name = 'player', 
+                                $select['players'], 
+                                set_value($field_name, $this->login_model->getInfo('uid')), 
+                                array(
+                                    'class' => 'form-control',
+                                )
+                            )?>
+                        </div>
                     </div>
                 </div>
+                <?php endif ?>
+                
+                <?php if (in_array($type, array('handover'))): ?>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-xs-6"><label>Chuyển từ:</label></div>
+                        <div class="col-xs-6"><label>đến:</label></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <?=form_dropdown(
+                                $field_name = 'player[0]', 
+                                $select['players'], 
+                                set_value($field_name, $this->login_model->getInfo('uid')), 
+                                array(
+                                    'class' => 'form-control',
+                                ) + $disabled_attr
+                            )?>
+                        </div>
+                        <div class="col-xs-6">
+                            <?=form_dropdown(
+                                $field_name = 'player[1]', 
+                                $select['players'], 
+                                set_value($field_name, 3-$this->login_model->getInfo('uid')), 
+                                array(
+                                    'class' => 'form-control',
+                                ) + $disabled_attr
+                            )?>
+                        </div>
+                    </div>
+                </div>
+                <?php endif ?>
+                
+                <div class="form-group">
+                    <label>Ghi chú:</label>
+                    <?=form_input(
+                        $field_name = 'memo', 
+                        set_value($field_name, null, false),
+                        array(
+                            'class' => 'form-control autocomplete',
+                        )
+                    )?>
+                </div>
+                <button type="submit" class="btn btn-primary">Nhập</button>
+                <?php if ($this->uri->segment(2) == 'edit'): ?>
+                    <a type="button" class="btn btn-danger pull-right del-record" href="<?=$del_url?>">Xóa</a>
+                <?php endif ?>
             </div>
-            <?php endif ?>
-            
-            <div class="form-group">
-                <label>Ghi chú:</label>
-                <?=form_input(
-                    $field_name = 'memo', 
-                    set_value($field_name, null, false),
-                    array(
-                        'class' => 'form-control autocomplete',
-                    )
-                )?>
-            </div>
-            <button type="submit" class="btn btn-primary">Nhập</button>
-            <?php if ($this->uri->segment(2) == 'edit'): ?>
-                <a type="button" class="btn btn-danger pull-right del-record" href="<?=$del_url?>">Xóa</a>
-            <?php endif ?>
         </div>
-    </div>
-</form>
+    </form>
+</div>
