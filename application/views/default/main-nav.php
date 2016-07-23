abdd989524ead8ff47df7b83af0d44f6b4813a0e
Nguyen Van Bach <vanbach1213@gmail.com>
2016-07-23 15:09:18 +0900

    Them chong CSRF cho xu ly logout

--- application/views/default/main-nav.php
+++ application/views/default/main-nav.php
@@ -3,7 +3,7 @@
 </script>
 
 <nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container" style="padding-left:0">
+    <div class="container fluid" style="padding-left:0">
         <div class="navbtn-group" id="mainNav">
             <a href="<?=base_url()?>"><span class="glyphicon glyphicon-home" style="padding:0 5px"></span></a>
             <a href="<?=base_url()?>inout/"><span class="glyphicon glyphicon-edit" style="padding:0 5px"></a>
@@ -12,11 +12,17 @@
         </div>
         <?php if ($this->login_model->isLogin()):?>
             <span class="navbar-text pull-right">
-                <a href="<?=base_url()?>user/dologout">
+                <a onclick="logout(); return false" href="">
                     <?=$this->login_model->getInfo('fullname')?> 
                     (Logout)
                 </a>
             </span>
+            <?php echo form_open(base_url().'user/dologout', array('id' => 'logOut', 'class' => 'pull-right')) ?>
+            </form>
+            
+            <script type="text/javascript">               
+                function logout(){$('form#logOut').submit()}
+            </script>
         <?php endif ?>
     </div>
 </nav>
