8d18fa826daf74545e9a2dab8834ce43ea4acca6
Nguyen Van Bach <vanbach1213@gmail.com>
2016-07-24 18:28:03 +0900

    Task #6: Feature: Tìm kiếm chi tiêu

--- application/views/default/search/search.php
+++ application/views/default/search/search.php
@@ -2,7 +2,7 @@
 <?php if ($list !== null):?>
     <div class="container">
         <?php if ($total_items > 0): ?>
-            <p>Tìm được <?=$total_items?> kết quả</p>
+            <p>Tìm được <strong><?=$total_items?></strong> kết quả</p>
         <?php endif ?>
         <?=$this->template->get_view('viewlist/inouts_of_day_list')?>
     </div>
