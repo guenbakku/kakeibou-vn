063158cc9c2e0aaa06941a26b2255daea974b6c3
Nguyen Van Bach <vanbach1213@gmail.com>
2016-08-09 10:10:24 +0900

    Chuyển url của nút Viewlist (Main Menu) đến thẳng danh sách trong tháng

--- application/views/default/main-nav.php
+++ application/views/default/main-nav.php
@@ -7,7 +7,7 @@
         <div class="navbtn-group" id="mainNav">
             <a href="<?=base_url()?>"><span class="glyphicon glyphicon-home" style="padding:0 5px"></span></a>
             <a href="<?=base_url()?>inout/"><span class="glyphicon glyphicon-edit" style="padding:0 5px"></a>
-            <a href="<?=base_url()?>viewlist/"><span class="glyphicon glyphicon-th-list" style="padding:0 5px"></span></a>
+            <a href="<?=base_url()?>viewlist/summary/day"><span class="glyphicon glyphicon-th-list" style="padding:0 5px"></span></a>
             <a href="<?=base_url()?>setting/"><span class="glyphicon glyphicon-cog" style="padding:0 5px"></span></a>
             <a href="<?=base_url()?>search/"><span class="glyphicon glyphicon-search" style="padding:0 5px"></span></a>
         </div>
