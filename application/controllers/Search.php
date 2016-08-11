a0f0ce9bdd913e03cdb90dfe7600e18bbac5896a
Nguyen Van Bach <vanbach1213@gmail.com>
2016-08-11 10:35:44 +0900

    Sửa lại cấu trúc hệ thống Controller

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -2,7 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Search extends MY_Controller {
-        
+    
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('search_model');
+    }
+    
 	public function index()
     {   
         $result = null;
