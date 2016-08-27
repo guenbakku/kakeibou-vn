787df3276358f94738ea0c6ac4aa63c5801c79ce
Nguyen Van Bach <vanbach1213@gmail.com>
2016-08-27 17:19:55 +0900

    Chuyển menu nhập thu chi thành dropdow menu trên Main menu

--- application/views/default/main-nav.php
+++ application/views/default/main-nav.php
@@ -6,7 +6,17 @@
     <div class="container fluid" style="padding-left:0">
         <div class="navbtn-group" id="mainNav">
             <a href="<?=base_url()?>"><span class="glyphicon glyphicon-home" style="padding:0 5px"></span></a>
-            <a href="<?=base_url()?>inout/"><span class="glyphicon glyphicon-edit" style="padding:0 5px"></a>
+            <span class="dropdown"href="<?=base_url()?>inout/"> <!-- For navigation color -->
+                <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+                    <span class="glyphicon glyphicon-edit" style="padding:0 5px">
+                </a>
+                <ul class="dropdown-menu">
+                    <?php $i=0 ?>
+                    <?php foreach (Inout_model::$CASH_FLOW_NAMES as $key => $item): ?>
+                        <li><a href="<?=base_url('inout/add/'.$key)?>"><?=(++$i)?>. <?=$item[0]?></a></li>
+                    <?php endforeach ?>
+                </ul>
+            </span>
             <a href="<?=base_url()?>viewlist/"><span class="glyphicon glyphicon-th-list" style="padding:0 5px"></span></a>
             <a href="<?=base_url()?>setting/"><span class="glyphicon glyphicon-cog" style="padding:0 5px"></span></a>
             <a href="<?=base_url()?>search/"><span class="glyphicon glyphicon-search" style="padding:0 5px"></span></a>
