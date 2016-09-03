762afdb717b541072bc8a8d1a701b9f04f979776
Nguyen Van Bach <vanbach1213@gmail.com>
2016-09-03 15:05:12 +0900

    Task #20: Feature: Tự scroll page đến thời điểm hiện tại khi xem danh sách thu chi (add blink background)

--- asset/upload/js/jquery.page-scroll.js
+++ asset/upload/js/jquery.page-scroll.js
@@ -2,22 +2,34 @@
  * Tự động cuộn trang (theo trục tung) đến vị trí của item được chỉ định
  */
 function pageScroll(target, offset, anchorName){
+        
     $(function(){
         
         if (target.length == 0){
             return false;
         }
         
-        offset = typeof offset !== 'undefined'? offset : 0;
-        anchorName = typeof anchorName !== 'undefined'? anchorName : 'page-scroll';
+        offset = typeof(offset) !== 'undefined'? offset : 0;
+        anchorName = typeof(anchorName) !== 'undefined'? anchorName : 'page-scroll';
         
         var targetDOM = $('['+anchorName+'='+target+']');
-        if (targetDOM.length > 0){
-            console.log($(document).scrollTop());
-            var y_coord = targetDOM.offset().top + offset;
-            $('body').animate({ scrollTop: y_coord }, "fast");
-        }
         
+        if (targetDOM.length > 0){
+            var y_coord = targetDOM.offset().top + offset;
+            $('body').animate({'scrollTop': y_coord }, {
+                'duration': 0,
+                'complete': blink(targetDOM),
+            });
+        }
+
         return false;
     })
+    
+    function blink(targetDOM){
+        orgColor = targetDOM.css('background-color');
+        blinkColor = '#ffff7a';
+        targetDOM.css('background-color', blinkColor).stop().animate({'backgroundColor' : orgColor}, {
+            'duration' : 1200,
+        });
+    }
 }
