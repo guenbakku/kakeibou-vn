5541282b3ccdeeb4278d0e8e4367277928f551bf
Nguyen Van Bach <vanbach1213@gmail.com>
2016-12-30 15:15:46 +0900

    Tinh gọn code phần install.php

--- _install/install.php
+++ _install/install.php
@@ -12,54 +12,25 @@ $DOMAIN   = $_SERVER['SERVER_NAME'];
 $APP_PATH = str_replace(INSTALL_PATH, '', $_SERVER['PHP_SELF']);
 $APP_URL  = 'http://'.$DOMAIN.$APP_PATH;
 
-// Danh sách những file cần thay đổi.
+// Danh sách file và những chỗ cần thay đổi trong file đó
 // Đường dẫn tương đối so với file php này.
 $files = array(
-    '../.htaccess',
-    '../index.php',
-    '../index.php',
-    '../application/config/database.php',
-    '../application/config/database.php',
-    '../application/config/database.php',
-    '../application/config/autoload.php',
+    '../.htaccess' => array(
+        "#CI_ENV (.*)#"         => 'production',
+        "#RewriteBase (.*)#"    => $APP_PATH,
+    ),
 );
-
-// Những chỗ cần thay đổi trong từng file.
-// Thứ tự dưới đây tương ứng với thứ tự file ở array $files
-$patterns = array(
-    "#RewriteBase (.*)#",
-    "#'BASEURL', '(.*)'#",
-    "#'ENVIRONMENT'.*'(development)#",
-    "#'database'.*=>.*'(.*)'#",
-    "#'username'.*=>.*'(.*)'#",
-    "#'password'.*=>.*'(.*)'#",
-    "#(,\s+'kint-0.9/kint')#",
-); 
-
-// Nội dung muốn thay đổi
-// Thứ tự dưới đây tương ứng với thứ tự file ở array $files
-$replaces = array(
-    $APP_PATH,
-    $APP_URL,
-    'production',
-    'kakeibou',
-    'guest',
-    'm4tkh4uDatabase',
-    '',
-);
-
-if (count($files) != count($patterns) || count($files) != count($replaces)){
-    die('ERROR: Dinh nghia thay doi khong hop le!');
-}
 
 // Thực hiện thay đổi
 echo 'APP_URL: '.$APP_URL.'<hr>';
-foreach($files as $i => $file){
-    $content = replace_specific($patterns[$i], $replaces[$i], file_get_contents($file));
-    file_put_contents($file, $content);
+foreach($files as $file => $replacements) {
+    foreach ($replacements as $pattern => $replace) {
+        $content = replace($pattern, $replace, file_get_contents($file));
+        file_put_contents($file, $content);
+    }
     echo $file.' -> OK<br>';
 }
-echo '<hr>SET URL CHO APP THANH CONG';
+echo '<hr>SET ENVIRONMENT THANH CONG';
 
 /*
  *--------------------------------------------------------------------
@@ -73,7 +44,7 @@ echo '<hr>SET URL CHO APP THANH CONG';
  * @return  string : nội dung đã thực hiện thay thế
  *--------------------------------------------------------------------
  */
-function replace_specific($pattern, $replace, $content)
+function replace($pattern, $replace, $content)
 {
     // Lật ngược cặp ngoặc trong pattern để đánh dấu phần cần giữ lại
     // Thêm ngoặc đóng 2 đầu
@@ -84,6 +55,5 @@ function replace_specific($pattern, $replace, $content)
     $pt = str_replace('((', ')', $pt);
     $pt = substr_replace($pt, '(', 1, 0);
     $pt = substr_replace($pt, ')', -1, 0);
-    
     return preg_replace($pt, "$1".$replace."$2", $content);
 }
