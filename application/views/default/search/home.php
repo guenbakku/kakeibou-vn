76966ac4f359a0836060e8a34549758e05965e35
Nguyen Van Bach <vanbach1213@gmail.com>
2017-01-03 23:05:03 +0900

    Task #2: thêm chức năng graph theo category

--- application/views/default/search/home.php
+++ application/views/default/search/home.php
@@ -1,9 +1,42 @@
 <?=$this->template->get_view('search/form')?>
+
 <?php if ($list !== null):?>
     <div class="container">
         <?php if ($total_items > 0): ?>
             <p>Tìm được <strong><?=$total_items?></strong> kết quả</p>
         <?php endif ?>
-        <?=$this->template->get_view('viewlist/inouts_of_day_list')?>
+        <div class="panel panel-default">
+            <?php if ($total_items > 0): ?>
+                <?php for ($i=0; $i<$total_items; $i++): ?>
+                    <?php if ($i==0 || $list[$i]['date'] != $list[$i-1]['date']): ?>
+                        <div class="panel-heading"><strong><?=$list[$i]['date']?> (<?=day_of_week($list[$i]['date'])?>)</strong></div>
+                        <div class="list-group">
+                    <?php endif ?>
+                            <!-- Item -->
+                            <a class="list-group-item" href="<?=sprintf($url['edit'], $list[$i]['id'])?>">
+                                <div style="padding-right:15px; position:absolute; right:0px">
+                                    <span class="glyphicon glyphicon-menu-right"></span>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-7" style="padding-right:0">
+                                        <div><?=$list[$i]['category']?></div>
+                                        <div class="small text-muted"><span class="fa <?=Account_model::$ICONS[$list[$i]['account_id']]?>"></span> <em><?=$list[$i]['memo']?></em></div>
+                                    </div>
+                                    <div class="col-xs-4 text-right">
+                                        <div class="<?=$list[$i]['inout_type']=='Thu'? 'text-income' : 'text-outgo'?>"><?=currency($list[$i]['amount'])?></div>
+                                        <div class="label <?=$list[$i]['player_label']?>"><?=$list[$i]['player']?></div>
+                                    </div>
+                                </div>
+                            </a>
+                    <?php if ($i==$total_items-1 || $list[$i]['date'] != $list[$i+1]['date']): ?>
+                        </div>
+                    <?php endif ?>
+                <?php endfor ?>
+            <?php else: ?>
+                <div class="panel-body">
+                    <?=Constants::ERR_NOT_FOUND?>
+                </div>
+            <?php endif ?>
+        </div>
     </div>
 <?php endif ?>
