849b1ae511e56520754755507c086e2bdb730ca0
Nguyen Van Bach <vanbach1213@gmail.com>
2017-03-26 16:04:15 +0900

    Task #51: Thay đổi cách hiển thị cột Dự định chi tiêu trong tháng

--- application/views/default/home/estimated_outgo_detail.php
+++ application/views/default/home/estimated_outgo_detail.php
@@ -2,16 +2,16 @@
     <tr>
         <td style="width:35%">Dự định:</td> 
         <td style="width:35%" class="text-right"><?=currency($estimated, false)?></td>
-        <td style="width:30%" class="text-right text-muted small"><?=$estimated_percent?>%</td>
+        <td style="width:30%" class="text-right text-muted"><?=$estimated_percent?>%</td>
     </tr>
     <tr>
         <td>Đã chi:</td> 
         <td class="text-right"><?=currency($elapsed, false)?></td>
-        <td class="text-right text-muted small"><?=$elapsed_percent?>%</td>
+        <td class="text-right text-muted"><?=$elapsed_percent?>%</td>
     </tr>
     <tr>
         <td>Còn lại:</td> 
         <td class="text-right"><?=currency($remain, false)?></td>
-        <td class="text-right text-muted small"><?=$remain_percent?>%</td>
+        <td class="text-right text-muted"><?=$remain_percent?>%</td>
     </tr>
 </table>
