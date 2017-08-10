7464730054b37b4a98e91809533b571fe19d31d1
Nguyen Van Bach <vanbach1213@gmail.com>
2017-08-10 16:15:47 +0900

    Feature #4: Thêm script phinx

---
+++ db/migrations/20170810060805_add_retrict_delete_flag_to_cash_account.php
@@ -0,0 +1,12 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddRetrictDeleteFlagToCashAccount extends AbstractMigration
+{
+    public function up()
+    {
+        $sql = "UPDATE accounts SET `restrict_delete`=1 WHERE `id`=1";
+        $this->execute($sql);
+    }
+}
---
+++ db/migrations/20170810060955_add_order_no_to_account_items.php
@@ -0,0 +1,17 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddOrderNoToAccountItems extends AbstractMigration
+{
+    public function up()
+    {
+        $sql = "SELECT * FROM accounts";
+        $rows = $this->fetchAll($sql);
+        
+        foreach ($rows as $i => $row) {
+            $sql = "UPDATE accounts SET `order_no`={$i} WHERE `id`={$row['id']}";
+            $this->execute($sql);
+        }
+    }
+}
