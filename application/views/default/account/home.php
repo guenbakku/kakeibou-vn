f501f06f8a35f3619e0680bc4e7c1eab99b407ec
Nguyen Van Bach <vanbach1213@gmail.com>
2017-08-10 17:52:03 +0900

    Feature #4: Tách setting của sortable sang 1 file js

--- application/views/default/account/home.php
+++ application/views/default/account/home.php
@@ -1,38 +1,8 @@
 <script type="text/javascript" src="<?=base_url()?>asset/upload/jquery-ui-1.12.0.custom/jquery.ui.touch-punch.min.js"></script>
+<script type="text/javascript" src="<?=base_url()?>asset/upload/js/app.sortableWrapper.js"></script>
 <script type="text/javascript">
     $(function(){
-        $('.sortable').sortable({
-            handle: '.handle',
-            items: '.sort',
-            axis: 'y',
-            start: function(event, ui) {
-                // Save start_pos of target item to use in below reorder
-                var start_pos = ui.item.index();
-                ui.item.data('start_pos', start_pos);
-            },
-            update: function(event, ui) {
-                var stop_pos = ui.item.index();
-                var start_pos = ui.item.data('start_pos');
-                var table = $(ui.item).parents('table');
-                
-                // we do not anything if position of target item does not change
-                if (start_pos == stop_pos) {
-                    return;
-                }
-                
-                // we only update order of items between start_pos and stop_pos
-                if (start_pos < stop_pos) {
-                    for(var i=stop_pos; i >= start_pos; i--){
-                        table.find('tr').eq(i).find('input[data-role=order_no]').val(i);
-                    }
-                }
-                else {
-                    for(var i=stop_pos; i <= start_pos; i++){
-                        table.find('tr').eq(i).find('input[data-role=order_no]').val(i);
-                    }
-                }
-            },
-        });
+        $('.sortable').sortableWrapper();
     });
 </script>
 
