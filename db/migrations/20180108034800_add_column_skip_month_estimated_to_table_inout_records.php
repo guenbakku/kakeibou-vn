d2aca942be395121866a52653fd422a874b57b15
Nguyen Van Bach <vanbach1213@gmail.com>
2018-01-08 12:56:24 +0900

    Feature #148: tạo script migration

---
+++ db/migrations/20180108034800_add_column_skip_month_estimated_to_table_inout_records.php
@@ -0,0 +1,61 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddColumnSkipMonthEstimatedToTableInoutRecords extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('inout_records');
+        $table->addColumn('skip_month_estimated', 'boolean', [
+            'after' => 'date',
+            'null' => false,
+            'default' => false,
+        ])
+        ->update();
+
+        $this->fillValue();
+    }
+
+    /**
+     * Fill value to new added column `skip_month_estimated`.
+     * Record which has category type is `skip_month_estimated` 
+     * will has value `true` in that column.
+     */
+    protected function fillValue()
+    {
+        $sql = "
+            SELECT `inout_records`.`id`, `categories`.`is_month_fixed_money` 
+            FROM `inout_records`
+            JOIN `categories` ON `categories`.`id` = `inout_records`.`category_id`
+        ";
+        $rows = $this->fetchAll($sql);
+        foreach ($rows as $row) {
+            if ($row['is_month_fixed_money'] == 1) {
+                $sql = "UPDATE inout_records SET skip_month_estimated=1 WHERE id={$row['id']}";
+                $this->execute($sql);
+            }
+        }
+    }
+}
