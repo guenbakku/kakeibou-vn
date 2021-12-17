51247df38a747073eb0995de2a4e435169af1567
Nguyen Van Bach <vanbach1213@gmail.com>
2021-12-17 22:10:04 +0700

    fix: remove unworked code

--- application/views/default/category/month_estimated_outgo.php
+++ application/views/default/category/month_estimated_outgo.php
@@ -35,7 +35,6 @@
                                             set_value($field_name, null),
                                             [
                                                 'class' => 'form-control amount',
-                                                'inputmode' => 'tel' // A cheet to show numeric keyboard in iOS
                                             ]
                                         )?>
                                         <span class="input-group-addon"><?=APP_CURRENCY?></span>
