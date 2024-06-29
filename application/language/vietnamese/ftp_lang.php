554a1b9b4892acb4cdcff2f7fbc79ac3ebe5cf37
Nguyen Van Bach <vanbach1213@gmail.com>
2024-06-29 14:35:34 +0700

    fix: update vietnamese translation

--- application/language/vietnamese/ftp_lang.php
+++ application/language/vietnamese/ftp_lang.php
@@ -1,18 +1,23 @@
 <?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author CodeIgniter community
+ * @copyright Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @link http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['ftp_no_connection']			= "Không tìm thấy ID kết nối. Hãy chắc chắn rằng bạn đã kết nối trước khi thực hiện các thao tác file.";
-$lang['ftp_unable_to_connect']		= "Không thể kết nối đến máy chủ FTP.";
-$lang['ftp_unable_to_login']		= "Không thể đăng nhập vào máy chủ FTP. Hãy kiểm tra lại tên và mật khẩu.";
-$lang['ftp_unable_to_makdir']		= "Không thể tạo thư mục.";
-$lang['ftp_unable_to_changedir']	= "Không thể thay đổi thư mục.";
-$lang['ftp_unable_to_chmod']		= "Không thể thiết lập quyền. Hãy kiểm tra lại đường dẫn. Chú ý: Tính năng này chỉ được hỗ trợ từ PHP 5 trở lên";
-$lang['ftp_unable_to_upload']		= "Không thể tải lên file. Hãy kiểm tra lại đường dẫn.";
-$lang['ftp_unable_to_download']		= "Không thể tải về file. Hãy kiểm tra lại đường dẫn.";
-$lang['ftp_no_source_file']			= "Không tìm thấy file nguồn. Hãy kiểm tra lại đường dẫn.";
-$lang['ftp_unable_to_rename']		= "Không thể đổi tên file.";
-$lang['ftp_unable_to_delete']		= "Không thẻ xóa file.";
-$lang['ftp_unable_to_move']			= "Không thể di chuyển file. Hãy chắc chắn rằng thư mục đích tồn tại.";
-
-
-/* End of file ftp_lang.php */
-/* Location: ./system/language/vietnamese/ftp_lang.php */
+$lang['ftp_no_connection'] = 'Không xác định ID kết nối - không hợp lệ, bạn hãy chắc chắn rằng đã kết nối trước khi thực hiện quy trình kết nối.';
+$lang['ftp_unable_to_connect'] = 'Không thể kết nối tới máy chủ, theo thông tin máy chủ bạn cung cấp.';
+$lang['ftp_unable_to_login'] = 'Không thể đăng nhập vào máy chủ. Vui lòng kiểm tra tên đăng nhập và mật khẩu';
+$lang['ftp_unable_to_mkdir'] = 'Không thể tạo thư mục mà bạn đã chỉ định.';
+$lang['ftp_unable_to_changedir'] = 'Không thể thay đổi thư mục.';
+$lang['ftp_unable_to_chmod'] = 'Không thể thiết lập quyền hạn tập tin. Vui lòng kiểm tra đường dẫn của bạn.';
+$lang['ftp_unable_to_upload'] = 'Không thể tải lên tập tin. Vui lòng kiểm tra đường dẫn của bạn.';
+$lang['ftp_unable_to_download'] = 'Không thể tải về tập tin. Vui lòng kiểm tra đường dẫn của bạn.';
+$lang['ftp_no_source_file'] = 'Không xác định được tập tin nguồn (Source file). Vui lòng kiểm tra đường dẫn của bạn.';
+$lang['ftp_unable_to_rename'] = 'Không thể đổi tên tập tin.';
+$lang['ftp_unable_to_delete'] = 'Không thể xóa tập tin.';
+$lang['ftp_unable_to_move'] = 'Không thể di chuyển tập tin. Hãy chắc chắn rằng thư mục đã được tạo.';
