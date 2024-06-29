42ba4c61d28e6517a2e95c0c8f786623d7bc6ddc
Nguyen Van Bach <vanbach1213@gmail.com>
2024-06-29 08:19:20 +0700

    fix: typo

--- asset/upload/js/app.js
+++ asset/upload/js/app.js
@@ -3,15 +3,15 @@
  */
 
 var Cashbook = {};
- 
+
 (function(Cashbook){
     /*
      * Quyết định mode cho form
      */
-    Cashbook.submitbutton = function(btn, type) {
+    Cashbook.submitButton = function(btn, type) {
         var form = $(btn).parents('form');
         var action = form.attr('action');
-        
+
         switch (type) {
             case 'submit':
             case 'add':
@@ -28,24 +28,24 @@ var Cashbook = {};
                 action = Cashbook.insertParam(action, 'delete', '1');
                 break;
         }
-        
+
         form.attr('action', action);
         form.submit();
     }
-    
+
     /*
      * Thêm param vào phần query của một url
      */
     Cashbook.insertParam = function (url, key, value) {
         key = encodeURI(key); value = encodeURI(value);
-                
-        var splited_url = url.split('?');
-        var domain = splited_url[0];
-        var kvp = typeof splited_url[1] == 'undefined'? [] : splited_url[1].split('&');
-        
+
+        var splitted_url = url.split('?');
+        var domain = splitted_url[0];
+        var kvp = typeof splitted_url[1] == 'undefined'? [] : splitted_url[1].split('&');
+
         var i=kvp.length; var x; while(i--) {
             x = kvp[i].split('=');
-    
+
             if (x[0]==key)
             {
                 x[1] = value;
@@ -53,9 +53,9 @@ var Cashbook = {};
                 break;
             }
         }
-    
+
         if(i<0) {kvp[kvp.length] = [key,value].join('=');}
-    
-        return domain + '?' + kvp.join('&'); 
+
+        return domain + '?' + kvp.join('&');
     }
 }(Cashbook));
