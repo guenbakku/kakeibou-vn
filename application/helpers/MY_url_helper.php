e6eeffe4800c7d246a83b521e600645d8554c6bd
Nguyen Van Bach <vanbach1213@gmail.com>
2024-07-06 10:18:14 +0700

    feat: implement `asset_url` helper

--- application/helpers/MY_url_helper.php
+++ application/helpers/MY_url_helper.php
@@ -1,14 +1,8 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-function my_site_url()
-{
-    $url = strtolower(site_url(func_get_args()));
-    return str_replace('/index.php', '', $url);
-}
-
 function query_string()
 {
-    return empty($_SERVER['QUERY_STRING'])? '' : '?'.$_SERVER['QUERY_STRING'];
+    return empty($_SERVER['QUERY_STRING']) ? '' : '?'.$_SERVER['QUERY_STRING'];
 }
 
 /**
@@ -21,4 +15,42 @@ function query_string()
 function template_url() {
     $CI =& get_instance();
     return $CI->template->template_url();
+}
+
+/**
+ * Generates a URL for a specified asset.
+ * Additionally, it appends a timestamp to the end of the URL
+ * to ensure the browser cache refreshes whenever the asset is updated.
+ *
+ * @param string $path The path to the asset within the `asset` folder (relative path).
+ * @param array $query The array containing the data to build query param
+ * @return string
+ */
+function asset_url(string $path, array $query = []): string {
+    $path = ltrim(normalize_url_path($path), '/');
+    $file_path = normalize_url_path(ASSETPATH . $path);
+    $modified_time = filemtime($file_path);
+
+    $query = http_build_query(array_merge(
+        $query,
+        ['t' => $modified_time],
+    ));
+    $url_path = base_url()."asset/$path?$query";
+    return $url_path;
+}
+
+function normalize_url_path(string $path): string {
+    return str_replace(
+        ['/', '\\'],
+        ['/', '/'],
+        $path,
+    );
+}
+
+function normalize_file_path(string $path): string {
+    return str_replace(
+        ['/', '\\'],
+        [DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR],
+        $path,
+    );
 }
--- application/views/default/master-layout.php
+++ application/views/default/master-layout.php
@@ -7,8 +7,8 @@
 <!-- Mobile first -->
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="vi">
-	<head>
-		<title><?=$_page_title?></title>
+    <head>
+        <title><?=$_page_title?></title>
         <meta http-equiv="Content-Type" content="Text/html; charset=utf-8" />
         <meta name="robots" content="noindex" />
         <meta name="description" content="<?=$_page_description?>" />
@@ -18,29 +18,29 @@
 
         <link rel="shortcut icon" href="<?=template_url()?>images/favicon.gif" type="image/gif">
         <link rel="apple-touch-icon" href="<?=template_url()?>images/favicon.gif" />
-        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=base_url()?>asset/upload/bootstrap-3.3.6-dist/css/bootstrap.min.css<?=$asset_time?>">
-        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=base_url()?>asset/upload/font-awesome-4.6.3/css/font-awesome.min.css<?=$asset_time?>">
-        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=base_url()?>asset/upload/jquery-ui-1.12.0.custom/jquery-ui.min.css<?=$asset_time?>">
-        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=base_url()?>asset/upload/jquery-ui-1.12.0.custom/jquery-ui.structure.min.css<?=$asset_time?>">
-        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=base_url()?>asset/upload/jquery-ui-1.12.0.custom/jquery-ui.theme.min.css<?=$asset_time?>">
+        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/bootstrap-3.3.6-dist/css/bootstrap.min.css')?>">
+        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/font-awesome-4.6.3/css/font-awesome.min.css')?>">
+        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/jquery-ui-1.12.0.custom/jquery-ui.min.css')?>">
+        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/jquery-ui-1.12.0.custom/jquery-ui.structure.min.css')?>">
+        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/jquery-ui-1.12.0.custom/jquery-ui.theme.min.css')?>">
         <link rel="stylesheet" type="text/css" media="screen,print" href="<?=template_url()?>css/theme.css<?=$asset_time?>">
         <link rel="stylesheet" type="text/css" media="screen,print" href="<?=template_url()?>css/main.css<?=$asset_time?>">
         <?=$_styles?>
 
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/js/jquery-1.12.3.min.js<?=$asset_time?>"></script>
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/bootstrap-3.3.6-dist/js/bootstrap.min.js<?=$asset_time?>"></script>
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/jquery-ui-1.12.0.custom/jquery-ui.min.js<?=$asset_time?>"></script>
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/js/app.js<?=$asset_time?>"></script>
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/js/app.utilities.js<?=$asset_time?>"></script>
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/js/app.navigation.js<?=$asset_time?>"></script>
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/js/app.page-scroll.js<?=$asset_time?>"></script>
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/js/app.switcher.js<?=$asset_time?>"></script>
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/waypoints/jquery.waypoints.min.js<?=$asset_time?>"></script>
-        <script type="text/javascript" src="<?=base_url()?>asset/upload/waypoints/infinite.min.js<?=$asset_time?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/js/jquery-1.12.3.min.js')?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/bootstrap-3.3.6-dist/js/bootstrap.min.js')?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/jquery-ui-1.12.0.custom/jquery-ui.min.js')?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/js/app.js')?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/js/app.utilities.js')?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/js/app.navigation.js')?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/js/app.page-scroll.js')?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/js/app.switcher.js')?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/waypoints/jquery.waypoints.min.js')?>"></script>
+        <script type="text/javascript" src="<?=asset_url('upload/waypoints/infinite.min.js')?>"></script>
         <?=$_scripts?>
-	</head>
+    </head>
 
-	<body>
+    <body>
         <?=$this->template->get_view('main-nav')?>
         <div class="container"><?=$this->flash->output()?></div>
         <?=$MAIN?>
