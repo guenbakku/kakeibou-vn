acf91a51c962866319662d0645e9e13ea104139b
Nguyen Van Bach <vanbach1213@gmail.com>
2024-07-06 13:42:53 +0700

    feat: implement `template_asset_url` helper

--- application/helpers/MY_url_helper.php
+++ application/helpers/MY_url_helper.php
@@ -1,20 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-function query_string()
-{
+function query_string() {
     return empty($_SERVER['QUERY_STRING']) ? '' : '?'.$_SERVER['QUERY_STRING'];
-}
-
-/**
- * Trả về url trỏ đến thư mục template đang được sử dụng.
- * Yêu cầu phải load sẵn library template
- *
- * @param   void
- * @return  string
- */
-function template_url() {
-    $CI =& get_instance();
-    return $CI->template->template_url();
 }
 
 /**
@@ -36,6 +23,34 @@ function asset_url(string $path, array $query = []): string {
         ['t' => $modified_time],
     ));
     $url_path = base_url()."asset/$path?$query";
+    return $url_path;
+}
+
+/**
+ * Generates a URL for a specified template asset.
+ * Different with `asset_url`, it handle the asset of current using template.
+ * Additionally, it appends a timestamp to the end of the URL
+ * to ensure the browser cache refreshes whenever the asset is updated.
+ *
+ * @param string $path The path to the asset within the `asset/{template}` folder (relative path).
+ * @param array $query The array containing the data to build query param
+ * @return string
+ */
+function template_asset_url(string $path, array $query = []): string {
+    $CI =& get_instance();
+    $path = ltrim(normalize_url_path($path), '/');
+    $file_path = normalize_url_path(
+        ASSETPATH
+        . $CI->template->folder
+        . $path
+    );
+    $modified_time = filemtime($file_path);
+
+    $query = http_build_query(array_merge(
+        $query,
+        ['t' => $modified_time],
+    ));
+    $url_path = $CI->template->template_url()."$path?$query";
     return $url_path;
 }
 
--- application/views/default/master-layout.php
+++ application/views/default/master-layout.php
@@ -16,15 +16,15 @@
         <meta name="copyright" content="© NVB" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-        <link rel="shortcut icon" href="<?=template_url()?>images/favicon.gif" type="image/gif">
-        <link rel="apple-touch-icon" href="<?=template_url()?>images/favicon.gif" />
+        <link rel="shortcut icon" type="image/gif" href="<?=template_asset_url('images/favicon.gif')?>">
+        <link rel="apple-touch-icon" href="<?=template_asset_url('images/favicon.gif')?>">
         <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/bootstrap-3.3.6-dist/css/bootstrap.min.css')?>">
         <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/font-awesome-4.6.3/css/font-awesome.min.css')?>">
         <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/jquery-ui-1.12.0.custom/jquery-ui.min.css')?>">
         <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/jquery-ui-1.12.0.custom/jquery-ui.structure.min.css')?>">
         <link rel="stylesheet" type="text/css" media="screen,print" href="<?=asset_url('upload/jquery-ui-1.12.0.custom/jquery-ui.theme.min.css')?>">
-        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=template_url()?>css/theme.css<?=$asset_time?>">
-        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=template_url()?>css/main.css<?=$asset_time?>">
+        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=template_asset_url('css/theme.css')?>">
+        <link rel="stylesheet" type="text/css" media="screen,print" href="<?=template_asset_url('css/main.css')?>">
         <?=$_styles?>
 
         <script type="text/javascript" src="<?=asset_url('upload/js/jquery-1.12.3.min.js')?>"></script>
