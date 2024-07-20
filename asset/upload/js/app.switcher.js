3a3eb2dc94c17f270e5aeca621f074a36b9d2f16
Nguyen Van Bach <vanbach1213@gmail.com>
2024-07-20 11:21:31 +0700

    feat: add onBeforeEnable and onBeforeDisable to app.switcher

--- asset/upload/js/app.switcher.js
+++ asset/upload/js/app.switcher.js
@@ -1,28 +1,30 @@
 /**
  * Disable/Enable input dựa vào giá trị hiện tại của một input khác
- * Hiện tại chỉ sử dụng được đối với những loại target sau: 
+ * Hiện tại chỉ sử dụng được đối với những loại target sau:
  *      input text, textarea, select.
  * Require:
  *      asset/upload/js/jquery-1.10.2.min.js (or higher version)
  */
-
 (function($){
     $.fn.switcher = function(options) {
         options = setDefaultOptions(options);
 
         // Define global properties
-        var props = {};
-        props.trigger     = this;
-        props.tmpAttrName = 'switcher-tmp';
-        
+        const props = {};
+        props.trigger = this;
+        props.targetBackupValueAttrName = 'switcher-target-backup-value';
+        props.triggerCurrentValueAttrName = 'switcher-trigger-current-value';
+
+        captureCurrentValueOfTrigger();
+
         // Setting cho sub switcher
         if (options.subSwitchers.length > 0) {
-            for (var i in options.subSwitchers) {
-                var subOptions = options.subSwitchers[i];
+            for (const i in options.subSwitchers) {
+                const subOptions = options.subSwitchers[i];
                 $(subOptions.selector).switcher(subOptions);
             }
         }
-        
+
         // Array-lize options values
         if (options.targets !== null && typeof options.targets !== 'object') {
             options.target = [options.targets];
@@ -33,24 +35,26 @@
         if (options.disableValues !== null && typeof options.disableValues !== 'object') {
             options.disableValues = [options.disableValues];
         }
-        
+
         // Here we go ...
         props.trigger.on('change', function(evt){
             if (options.targets === null) {
                 console.error('targets must be defined');
                 return;
             }
-            
-            if (options.enableValues === null 
+
+            if (options.enableValues === null
                 && options.disableValues === null)
             {
                 console.error('enableValues or disableValues must be defined');
                 return;
             }
-            
+
             // Check is enable or not
-            var triggerVal = props.trigger.val();
-            var isEnable = false;
+            const previousVal = getPreviousValueOfTrigger();
+            const triggerVal = props.trigger.val();
+            let isEnable = false;
+
             if (options.enableValues !== null) {
                 isEnable = options.enableValues.indexOf(triggerVal) >= 0
                          ? true : false;
@@ -58,99 +62,127 @@
                 isEnable = options.disableValues.indexOf(triggerVal) >= 0
                          ? false : true;
             }
-            
+
             // Switch target depend on isEnable value
             if (isEnable) {
-                enable(options.targets);
+                enable(options.targets, triggerVal, previousVal);
                 triggerSubSwitchers();
             } else {
-                disable(options.targets);
+                disable(options.targets, triggerVal, previousVal);
                 disableAllSubSwitchers();
             }
+
+            captureCurrentValueOfTrigger();
         }).trigger('change');
-        
-        return this;
-        
+
+        return props.trigger;
+
         //---------------------------------------------------
         // Helper method
         //---------------------------------------------------
-        
+        function captureCurrentValueOfTrigger() {
+            props.trigger.attr(props.triggerCurrentValueAttrName, props.trigger.val());
+        }
+
+        function getPreviousValueOfTrigger() {
+            return props.trigger.attr(props.triggerCurrentValueAttrName);
+        }
+
         function backupCurrentValue(selector) {
             $(selector).each(function(i, dom){
                 // Skip if this target's value is already backed up
-                if ($(dom).is('['+props.tmpAttrName+']')) {
+                if ($(dom).is('['+props.targetBackupValueAttrName+']')) {
                     return false;
                 }
-                var currentVal = $(dom).val();
-                $(dom).attr(props.tmpAttrName, currentVal);
+                const currentVal = $(dom).val();
+                $(dom).attr(props.targetBackupValueAttrName, currentVal);
             });
         }
-        
+
         function restoreCurrentValue(selector) {
             $(selector).each(function(i, dom){
                 // Skip if this target's value is not backed up
-                if (!$(dom).is('['+props.tmpAttrName+']')) {
+                if (!$(dom).is('['+props.targetBackupValueAttrName+']')) {
                     return false;
                 }
-                var currentVal = $(dom).attr(props.tmpAttrName);
-                $(dom).val(currentVal);
-                $(dom).removeAttr(props.tmpAttrName);
+
+                const currentVal = $(dom).attr(props.targetBackupValueAttrName);
+
+                // Only restore if the currentVal is one of <option> tag's value
+                $(dom).children('option').each(function(){
+                    if (this.value == currentVal) {
+                        $(dom).val(currentVal);
+                        return false;
+                    }
+                });
+
+                $(dom).removeAttr(props.targetBackupValueAttrName);
             });
         }
-        
-        function disable(targets) {
-            for (var i in targets) {
-                var selector = targets[i];
-                backupCurrentValue(selector);
-                $(selector).each(function(i, dom){
-                    $(dom).val(null);
-                    $(dom).prop('disabled', true);
-                });
+
+        function disable(targets, triggerVal, previousVal) {
+            const result = options.onBeforeDisable(triggerVal, previousVal);
+            if (result) {
+                for (const i in targets) {
+                    const selector = targets[i];
+                    backupCurrentValue(selector);
+                    $(selector).each(function(i, dom){
+                        $(dom).val(null);
+                        $(dom).prop('disabled', true);
+                    });
+                }
             }
         }
-        
-        function enable(targets) {
-            for (var i in targets) {
-                var selector = targets[i];
-                restoreCurrentValue(selector);
-                $(selector).each(function(i, dom){
-                    $(dom).prop('disabled', false);
-                });
+
+        function enable(targets, triggerVal, previousVal) {
+            const result = options.onBeforeEnable(triggerVal, previousVal);
+            if (result) {
+                for (const i in targets) {
+                    const selector = targets[i];
+                    restoreCurrentValue(selector);
+                    $(selector).each(function(i, dom){
+                        $(dom).prop('disabled', false);
+                    });
+                }
             }
         }
-        
+
         function triggerSubSwitchers() {
-            for (var i in options.subSwitchers) {
-                var subOptions = options.subSwitchers[i];
+            for (const i in options.subSwitchers) {
+                const subOptions = options.subSwitchers[i];
                 $(subOptions.selector).trigger('change');
             }
         }
-        
+
         function disableAllSubSwitchers() {
-            for (var i in options.subSwitchers) {
-                var subOptions = options.subSwitchers[i];
+            for (const i in options.subSwitchers) {
+                const subOptions = options.subSwitchers[i];
                 disable(subOptions.targets);
             }
         }
-        
+
         function setDefaultOptions(options) {
             options = typeof options === 'object'
-                      ? options : {};
-            options.targets       = typeof options.targets !== 'undefined'
-                                    ? options.targets : null;
-            options.enableValues  = typeof options.enableValues !== 'undefined'
-                                    ? options.enableValues : null;
+                ? options : {};
+            options.targets = typeof options.targets !== 'undefined'
+                ? options.targets : null;
+            options.enableValues = typeof options.enableValues !== 'undefined'
+                ? options.enableValues : null;
             options.disableValues = typeof options.disableValues !== 'undefined'
-                                    ? options.disableValues : null;
-            options.subSwitchers  = typeof options.subSwitchers === 'object'
-                                    ? options.subSwitchers : [];
-            options.selector      = typeof options.selector === 'string'
-                                    ? options.selector : null;
-            
-            for (var i in options.subSwitchers) {
+                ? options.disableValues : null;
+            options.subSwitchers = typeof options.subSwitchers === 'object'
+                ? options.subSwitchers : [];
+            options.selector = typeof options.selector === 'string'
+                ? options.selector : null;
+            options.onBeforeDisable = typeof options.onBeforeDisable === 'function'
+                ? options.onBeforeDisable : () => true;
+            options.onBeforeEnable = typeof options.onBeforeEnable === 'function'
+                ? options.onBeforeEnable : () => true;
+
+            for (const i in options.subSwitchers) {
                 options.subSwitchers[i] = setDefaultOptions(options.subSwitchers[i]);
             }
-            
+
             return options;
         }
     }
