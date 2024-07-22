28b00c644ab60517464d6af40e773de7d6d805f6
Nguyen Van Bach <vanbach1213@gmail.com>
2024-07-22 20:43:30 +0700

    fix: clear inputted date on iOS

--- asset/upload/js/app.utilities.js
+++ asset/upload/js/app.utilities.js
@@ -1,19 +1,31 @@
 /**
  * Utilities
  */
- 
 $(function(){
-    /*
+    /**
      * Xóa class 'active' của những item không được click trong cùng 1 button group
      */
-    $('.btn-group .btn').click(function(){ 
+    $('.btn-group .btn').click(function(){
         $(this).addClass('active').siblings().removeClass('active');
-    }); 
-    
-    /*
+    });
+
+    /**
      * Submit form khi input có class .submit-on-change thay đổi giá trị
      */
     $('.submit-on-change').change(function(){
         $(this).parents('form').submit();
+    });
+
+    /**
+     * Workaround to make the Reset button of Datepicker on iOS can clear the inputted value to blank.
+     * Many thanks to: https://stackoverflow.com/a/64886383
+     */
+    $(window).on("load", function(){
+        const dateFields = document.querySelectorAll('input[type="date"]');
+        for (const dateField of dateFields) {
+            const value = dateField.value;
+            dateField.defaultValue = '';
+            dateField.value = value;
+        }
     });
 })
