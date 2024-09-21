1f4a17612d41718b19f68f104e2d33e4c8f2351a
Nguyen Van Bach <vanbach1213@gmail.com>
2024-09-21 09:27:25 +0700

    refactor: add function existence checking to helpers definition

--- application/helpers/MY_array_helper.php
+++ application/helpers/MY_array_helper.php
@@ -4,82 +4,88 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-/**
- * Trả về array mới có index là giá trị của một column cụ thể có trong array.
- *
- * @param null|array  $arr array có ít nhất 2 chiều
- * @param null|string $key tên một trong những column trong array. Index của array mới sẽ được tạo từ các giá trị trong column này.
- * @param null|string $val tên một trong những column trong array.
- *                         - Nếu biến này được quy định, array sẽ được tạo với dạng array(key => value)
- *                         với value là giá trị trong cột được quy định ở biến này
- *                         - Nếu biến này là null, array sẽ được tạo với dạng array(key => item)
- *                         với item là item nguyên bản của array truyền vào
- *
- * @return array array mới đã được đánh index
- */
-function array_gen_key($arr = null, $key = null, $val = null)
-{
-    if (!is_array($arr)) {
-        return false;
-    }
-
-    // Kiểm tra sự tồn tại của colum trong array
-    $first_item = reset($arr);
-    if (!isset($first_item[$key]) || !is_string($first_item[$key])) {
-        return $arr;
-    }
-    if ($val !== null && !isset($first_item[$val])) {
-        return $arr;
-    }
-
-    $new_arr = [];
-    if ($val === null) {
-        foreach ($arr as $i => $item) {
-            $new_arr[$item[$key]] = $item;
+if (!function_exists('array_gen_key')) {
+    /**
+     * Trả về array mới có index là giá trị của một column cụ thể có trong array.
+     *
+     * @param null|array  $arr array có ít nhất 2 chiều
+     * @param null|string $key tên một trong những column trong array. Index của array mới sẽ được tạo từ các giá trị trong column này.
+     * @param null|string $val tên một trong những column trong array.
+     *                         - Nếu biến này được quy định, array sẽ được tạo với dạng array(key => value)
+     *                         với value là giá trị trong cột được quy định ở biến này
+     *                         - Nếu biến này là null, array sẽ được tạo với dạng array(key => item)
+     *                         với item là item nguyên bản của array truyền vào
+     *
+     * @return array array mới đã được đánh index
+     */
+    function array_gen_key($arr = null, $key = null, $val = null)
+    {
+        if (!is_array($arr)) {
+            return false;
         }
-    } else {
-        foreach ($arr as $i => $item) {
-            $new_arr[$item[$key]] = $item[$val];
-        }
-    }
 
-    return $new_arr;
+        // Kiểm tra sự tồn tại của colum trong array
+        $first_item = reset($arr);
+        if (!isset($first_item[$key]) || !is_string($first_item[$key])) {
+            return $arr;
+        }
+        if ($val !== null && !isset($first_item[$val])) {
+            return $arr;
+        }
+
+        $new_arr = [];
+        if ($val === null) {
+            foreach ($arr as $i => $item) {
+                $new_arr[$item[$key]] = $item;
+            }
+        } else {
+            foreach ($arr as $i => $item) {
+                $new_arr[$item[$key]] = $item[$val];
+            }
+        }
+
+        return $new_arr;
+    }
 }
 
-/**
- * Update key của array_1 bằng key của array_2.
- * Chỉ update những key có mặt ở cả array_1 và array_2.
- * Những key có ở array_1 nhưng không có ở array_2 sẽ được giữ nguyên giá trị.
- *
- * @param array $array1 array gốc
- * @param array $array2 array chứa nội dung muốn thay đổi cho array_1
- *
- * @return array array_1 đã được update
- */
-function array_update(array $array1, array $array2): array
-{
-    foreach ($array1 as $key => $val) {
-        if (isset($array2[$key])) {
-            $array1[$key] = $array2[$key];
+if (!function_exists('array_update')) {
+    /**
+     * Update key của array_1 bằng key của array_2.
+     * Chỉ update những key có mặt ở cả array_1 và array_2.
+     * Những key có ở array_1 nhưng không có ở array_2 sẽ được giữ nguyên giá trị.
+     *
+     * @param array $array1 array gốc
+     * @param array $array2 array chứa nội dung muốn thay đổi cho array_1
+     *
+     * @return array array_1 đã được update
+     */
+    function array_update(array $array1, array $array2): array
+    {
+        foreach ($array1 as $key => $val) {
+            if (isset($array2[$key])) {
+                $array1[$key] = $array2[$key];
+            }
         }
-    }
 
-    return $array1;
+        return $array1;
+    }
 }
 
-/**
- * Chuyển 1 associate array (key-value) thành 1 number-index array (list).
- * Các phần tử của list này là 1 array con, mỗi array con chứa 2 phần tử có giá trị của `key` và `value`.
- */
-function array_to_list(array $arr)
-{
-    return array_map(
-        function ($v, $k) {
-            return [$k, $v];
-        },
-        $arr,
-        array_keys($arr),
-    );
+if (!function_exists('array_to_list')) {
+    /**
+     * Chuyển 1 associate array (key-value) thành 1 number-index array (list).
+     * Các phần tử của list này là 1 array con, mỗi array con chứa 2 phần tử có giá trị của `key` và `value`.
+     */
+    function array_to_list(array $arr)
+    {
+        return array_map(
+            function ($v, $k) {
+                return [$k, $v];
+            },
+            $arr,
+            array_keys($arr),
+        );
+    }
 }
 
 // End of file MY_date_helper.php
--- application/helpers/currency_helper.php
+++ application/helpers/currency_helper.php
@@ -4,13 +4,15 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-function currency($num, bool $plus_sign = true)
-{
-    if ($plus_sign === true) {
-        $sign = $num > 0 ? '+' : '';
-    } else {
-        $sign = '';
-    }
+if (!function_exists('currency')) {
+    function currency($num, bool $plus_sign = true)
+    {
+        if ($plus_sign === true) {
+            $sign = $num > 0 ? '+' : '';
+        } else {
+            $sign = '';
+        }
 
-    return $sign.number_format($num);
+        return $sign.number_format($num);
+    }
 }
