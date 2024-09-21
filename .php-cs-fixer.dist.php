9168e95f0dd0079cc3894624bbc6a86728501498
Nguyen Van Bach <vanbach1213@gmail.com>
2024-09-21 10:16:46 +0700

    chore: delete unnecessary php-cs-fixer rule `braces`

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -18,7 +18,6 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PhpCsFixer' => true,
         'echo_tag_syntax' => ['format' => 'short'],
-        'braces' => ['position_after_functions_and_oop_constructs' => 'same'],
         'binary_operator_spaces' => ['default' => 'single_space', 'operators' => ['=>' => 'single_space']],
         'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
     ])
