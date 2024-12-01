d68b6aa6a7afcbac55079f3e4f9309f2475efe0a
Nguyen Van Bach <vanbach1213@gmail.com>
2024-12-01 16:21:04 +0700

    chore: bump app version to 3.2.1

--- application/libraries/Settings.php
+++ application/libraries/Settings.php
@@ -9,7 +9,7 @@ class Settings
     public function __construct()
     {
         $this->storage = [
-            'version' => '3.2.0',
+            'version' => '3.2.1',
             'currency' => $_ENV['CURRENCY'] ?? 'VND',
             'err_bad_request' => 'Yêu cầu không hợp lệ',
             'err_not_found' => 'Không tìm thấy dữ liệu',
